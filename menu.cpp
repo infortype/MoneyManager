52823426f8f037284e3d8bf8d669c53fc9433bd0
OmarLenovo <omarseguraarevalo@gmail.com>
2022-04-10 19:14:22 +0100

    Version 2

--- menu.cpp
+++ menu.cpp
@@ -3,14 +3,19 @@
 #include "server/server.h"
 
 #include <memory>
+#include <QMessageBox>
 
 menu::menu(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::menu)
 {
     ui->setupUi(this);
+
     myServer = std::make_unique<Server>("MoneyManager.sqlite");
     myServer->ConnectToBase();
+
+    myTable1 = std::make_unique<Table>("Sheets");
+     if(!ConfigureTable1()) QMessageBox::critical(this, "Error", "Table error");
 }
 
 menu::~menu()
@@ -18,3 +23,10 @@ menu::~menu()
     delete ui;
 }
 
+bool menu::ConfigureTable1() const noexcept
+{
+
+    myTable1->AddRow("NameSheet", Server::Text, "Primary Key unique");
+    return myTable1->Create();
+
+}
--- menu.h
+++ menu.h
@@ -9,6 +9,7 @@ namespace Ui { class menu; }
 QT_END_NAMESPACE
 
 class Server;
+class Table;
 
 class menu : public QMainWindow
 {
@@ -21,6 +22,9 @@ public:
 private:
     Ui::menu *ui;
     std::unique_ptr<Server>myServer;
+    std::unique_ptr<Table>myTable1;
+
+    [[nodiscard]] bool ConfigureTable1() const noexcept;
 
 };
 #endif // MENU_H
